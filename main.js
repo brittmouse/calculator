d502739282e4bbd4a9e1f3dfd540b71a948d63bc
brittmouse <bstancoff@proton.me>
2023-11-08 17:42:22 -0500

    Add basic math functions

---
+++ main.js
@@ -0,0 +1,6 @@
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+console.log(add(5, 3), subtract(3, 5), multiply(3, 5), divide(3, 5));
