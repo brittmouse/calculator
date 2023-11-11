6666880872a3340e2ba15097dfa705f2a1eb9067
brittmouse <bstancoff@proton.me>
2023-11-10 19:32:01 -0500

    !BUGGY! - Add event listeners to numbers and decimal point, add "Clear" button
    For some reason, using a loop to assign event listeners to all of the ".num"
    divs causes the JS to execute *on load* instead of when the div is clicked as
    is called with the "click" parameter - I may just need to change the divs to
    buttons but that's a big enough change for me to want to do it as a separate
    commit.

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const display = document.querySelector(".display");
+display.innerHTML = "";
+
+const numButtons = document.querySelectorAll(".num");
+for (let i = 0; i < numButtons.length; i++) {
+  numButtons[i].addEventListener("click", handleNumClick(numButtons[i].innerHTML));
+}
+
+function handleNumClick(text) {
+  // if (text !== ".") Number(text);
+  display.innerHTML += text;
+}
+
 const operate = function(x, operator, y) {
   if (operator === "+") return add(x, y);
   if (operator === "-") return subtract(x, y);
@@ -9,9 +22,3 @@ const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
-
-let num1 = 3;
-let num2 = 5;
-let operator;
-
-console.log(add(5, 3), subtract(3, 5), multiply(3, 5), divide(3, 5));
